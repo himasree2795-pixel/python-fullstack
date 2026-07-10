0ff601924ea52e30e8fe70951550b6c647f252f7
Himasree <himasree2795@gmail.com>
2026-07-10 15:20:19 +0530

    day4

---
+++ main.py
@@ -0,0 +1,18 @@
+import calculator
+
+result = calculator.add(10,20)
+print(result)
+
+from calculator import add
+print(add(10,20))
+
+import calculator as cal
+print(cal.add(10,20))
+
+import math
+print(math.sqrt(15))
+
+import random
+number = random.randint(1,10)
+print(number)
+
